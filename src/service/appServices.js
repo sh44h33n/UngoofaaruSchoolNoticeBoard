d7fc743e755f2bde86cbb3fbcd4e8ca15f526160
Shaheen Mohamed <shaaheen.mv@gmail.com>
2017-02-01 22:03:15 +0500

    Second

--- src/service/appServices.js
+++ src/service/appServices.js
@@ -3,37 +3,16 @@
 
 
 app.service('appService', function() {
-    var foodData = new Array();
+    var UngoofaaruSchoolNotice = new Array();
 
 
     //food array for adding to food page from services...
 
 
-    var foodData = [{
-
-            "title": "Watermelon",
-            "id": "10",
-            "desc": "Healthy fruit",
-            "protien": "0.6 gram protien",
-            "fats": "0.9 gram fats",
-            "energy": "658KJ energy",
-            "image": "melon.jpg"
-        },
-        {
-
-            "title": "Mango",
-            "id": "11",
-            "desc": "Healthy fruit",
-            "protien": "0.9 gram protien",
-            "fats": "1.9 gram fats",
-            "energy": "958KJ energy",
-            "image": "mango.jpg"
-        }
-    ]
-
+   
     return {
-        getnewFoods: function() {
-            return foodData; //return food array
+        getNotice: function() {
+            return UngoofaaruSchoolNotice; //return all notice
         },
 
 
