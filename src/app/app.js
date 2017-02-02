51ca3b43d2981122f721eb79899e047fc74cdfed
Shaheen Mohamed <shaaheen.mv@gmail.com>
2017-02-02 11:28:12 +0500

    Third

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,7 @@ var app = angular.module('UngoofaaruSchool', ['ui.router', 'ui.bootstrap']);
 app.config(function($stateProvider) {
     $stateProvider
         .state('index', {
-            url: "",
+            url: '',
             views: {
                 "schoollogo": {
                     templateUrl: './views/Schoollogo.html',
@@ -38,6 +38,75 @@ app.config(function($stateProvider) {
                 },
                 "englishscroller": {
                     templateUrl: './views/BottomEnglishScroll.html',
+                    controller: ''
+                }
+            }
+        })
+        .state('brand', {
+            url: '/brand',
+            views: {
+                "schoollogo": {
+                    templateUrl: './views/Schoollogo.html',
+                    controller: ''
+
+                },
+                "schoolmoto": {
+                    templateUrl: './views/Schoolmoto.html',
+                    controller: ''
+                },
+                "currentdate": {
+                    templateUrl: './views/Currentdate.html',
+                    controller: ''
+
+                },
+                "occasionallogo": {
+                    templateUrl: './views/Occationallogo.html',
+                    controller: ''
+                },
+                "schoolnotice": {
+                    templateUrl: './views/LeftSchoolNotice.html',
+                    controller: ''
+
+                },
+                "schoolvideo": {
+                    templateUrl: './views/RightSchoolVideo.html',
+                    controller: ''
+                },
+                "dhivehiscroller": {
+                    templateUrl: './views/BottomDhivehiScroll.html',
+                    controller: ''
+
+                },
+                "englishscroller": {
+                    templateUrl: './views/BottomEnglishScroll.html',
+                    controller: ''
+                }
+            }
+        })
+        .state('login', {
+            url: '/login',
+            views: {
+                "login": {
+                    templateUrl: './views/login.html',
+                    controller: ''
+
+                },
+                "badge": {
+                    templateUrl: './views/Schoolmoto.html',
+                    controller: ''
+                }
+            }
+        })
+        .state('dashboard', {
+            url: '/dashboard',
+            views: {
+
+                "dashboard": {
+                    templateUrl: './views/dashboard.html',
+                    controller: ''
+                },
+                "reports": {
+                    templateUrl: './views/reports.html',
                     controller: ''
                 }
             }
