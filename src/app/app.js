f89c2f95dc5f3eab35e1ab73f70793d72e75522c
Shaheen Mohamed <shaaheen.mv@gmail.com>
2017-02-03 20:34:25 +0500

    Hello

--- src/app/app.js
+++ src/app/app.js
@@ -132,3 +132,15 @@ app.config(function($stateProvider) {
 
 
 })
+app.controller('MainCtrl', function($scope) {
+    $scope.name = 'World';
+});
+
+app.controller('DropdownCtrl', function($scope) {
+ 
+    $scope.items = [
+        "The first choice!",
+        "And another choice for you.",
+        "but wait! A third!"
+    ];
+});
